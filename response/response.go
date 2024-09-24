ca1f5b43c62ac91923704373ea40ef669d855842
Guo Xiyao <3540069440@qq.com>
2024-09-24 22:46:13 +0800

    1.非空不返回 2.多余的返回删除

--- response/response.go
+++ response/response.go
@@ -3,6 +3,7 @@ package response
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"net/http"
 )
 
@@ -24,4 +25,32 @@ func NewResponse(code int, data interface{}, message string) ApiResponse {
 // WriteJSON 将 ApiResponse 写入 HTTP 响应
 func WriteJSON(c *gin.Context, response ApiResponse) {
 	c.JSON(http.StatusOK, response)
+}
+
+// DiaryResponse 用于API响应的日记信息
+type DiaryResponse struct {
+	ID        uint           `json:"id"`
+	CreatedAt string         `json:"createdAt,omitempty"`
+	UpdatedAt string         `json:"updatedAt,omitempty"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
+	UserID    uint           `json:"userID"`
+	Content   string         `json:"content"`
+	User      UserResponse   `json:"user,omitempty"`
+	Tags      []TagResponse  `json:"tags,omitempty"`
+}
+
+// UserResponse 用于API响应的用户信息
+type UserResponse struct {
+	ID          uint   `json:"id"`
+	PhoneNumber string `json:"phoneNumber,omitempty"`
+	Password    string `json:"password,omitempty"`
+}
+
+// TagResponse 用于API响应的标签信息
+type TagResponse struct {
+	ID        uint           `json:"id"`
+	CreatedAt string         `json:"createdAt,omitempty"`
+	UpdatedAt string         `json:"updatedAt,omitempty"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
+	Name      string         `json:"name"`
 }
